adb901892d4fb340883a031bd333c502ac361fbc
Mihail-Ivannikov <116758780+Mihail-Ivannikov@users.noreply.github.com>
2024-06-06 23:31:29 +0300

    Update client.go

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -13,14 +13,17 @@ var target = flag.String("target", "http://localhost:8090", "request target")
 func main() {
 	flag.Parse()
 	client := &http.Client{
-		Timeout: 10 * time.Second,
+		Timeout: 10 * time.Second, // Set a timeout
 	}
-
+	// Loop every second
 	for range time.Tick(1 * time.Second) {
+		// Send a GET request to the target URL
 		resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", *target))
 		if err == nil {
+			// If no error, print the response status code
 			log.Printf("response %d", resp.StatusCode)
 		} else {
+			// If there's an error, print the error message
 			log.Printf("error %s", err)
 		}
 	}
