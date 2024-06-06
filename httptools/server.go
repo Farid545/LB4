f7d9f7fdb364992268437f23154b12831b6ac4e8
Mihail-Ivannikov <116758780+Mihail-Ivannikov@users.noreply.github.com>
2024-06-06 23:26:56 +0300

    Update server.go

--- httptools/server.go
+++ httptools/server.go
@@ -7,6 +7,7 @@ import (
     "time"
 )
 
+// Server interface with a Start method
 type Server interface {
     Start()
 }
@@ -17,20 +18,21 @@ type server struct {
 
 func (s server) Start() {
     go func() {
-        log.Println("Starting the HTTP server...")
-        err := s.httpServer.ListenAndServe()
-        log.Fatalf("HTTP server finished: %s. Finishing the process.", err)
+        log.Println("Starting the HTTP server...") // Print a message that the server is starting
+        err := s.httpServer.ListenAndServe() // Start the server
+        log.Fatalf("HTTP server finished: %s. Finishing the process.", err) // Print an error message if the server stops
     }()
 }
 
+// CreateServer function makes a new Server
 func CreateServer(port int, handler http.Handler) Server {
     return server{
         httpServer: &http.Server{
-            Addr:           fmt.Sprintf(":%d", port),
-            Handler:        handler,
-            ReadTimeout:    10 * time.Second,
-            WriteTimeout:   10 * time.Second,
-            MaxHeaderBytes: 1 << 20,
+            Addr:           fmt.Sprintf(":%d", port), // Set the server address
+            Handler:        handler, // Set the request handler
+            ReadTimeout:    10 * time.Second, // Set the read timeout to 10 seconds
+            WriteTimeout:   10 * time.Second, // Set the write timeout to 10 seconds
+            MaxHeaderBytes: 1 << 20, // Set the max header size
         },
     }
 }
